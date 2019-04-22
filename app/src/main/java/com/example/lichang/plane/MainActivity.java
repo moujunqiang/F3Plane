794f6e72c22549707f7b920e32f16df418653e01
stanhuo <2421651879@qq.com>
2019-04-22 11:02:53 +0800

    背景音子弹音按钮

--- app/src/main/java/com/example/lichang/plane/MainActivity.java
+++ app/src/main/java/com/example/lichang/plane/MainActivity.java
@@ -11,17 +11,15 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    private Button button1, button2, button3, button4;
-    public static List <Activity> activityList = new LinkedList ();
+    private Button button1, button3;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate ( savedInstanceState );
         setContentView ( R.layout.activity_main );
         button1 = findViewById ( R.id.start );
-        button2 = findViewById ( R.id.online );
         button3 = findViewById ( R.id.rank );
-        button4 = findViewById ( R.id.back );
         button1.setOnClickListener ( new View.OnClickListener () {
             @Override
             public void onClick(View v) {
@@ -29,13 +27,7 @@ public class MainActivity extends AppCompatActivity {
                 startActivity ( intent );
             }
         } );
-        button2.setOnClickListener ( new View.OnClickListener () {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent ( MainActivity.this, Online.class );
-                startActivity ( intent );
-            }
-        } );
+
         button3.setOnClickListener ( new View.OnClickListener () {
             @Override
             public void onClick(View v) {
@@ -43,18 +35,6 @@ public class MainActivity extends AppCompatActivity {
                 startActivity ( intent );
             }
         } );
-        button4.setOnClickListener ( new View.OnClickListener () {
-            @Override
-            public void onClick(View v) {
-                exit ();
-            }
-        } );
-    }
 
-    public void exit() {
-        for (Activity act : activityList) {
-            act.finish ();
-        }
-        System.exit ( 0 );
     }
 }
